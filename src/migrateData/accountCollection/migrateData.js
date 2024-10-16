a18a5ed81d2b6a62bb0faf1668129d23c6c3c3c8
saman@1370 <zappfleet@gmail.com>
2024-10-16 17:22:48 +0400

    1403/07/25

--- src/migrateData/accountCollection/migrateData.js
+++ src/migrateData/accountCollection/migrateData.js
@@ -7,14 +7,15 @@ const { ObjectId } = mongoose.Types;
 const config = require("config");
 const { userStatus } = require('../../users/data/constants/userStatus');
 
-
+// port  27048
+//mongodump --uri="mongodb://localhost:27048/zappfleet" --out="C:\MongoDB_Backups"
 // Function to connect to MongoDB
 const connectToDB = async () => {
     const environment_name = config.get("environment_name");
     let db = "";
 
     if (environment_name === "local") {
-        db = process.env.DATABASE_URL// config.get("db");
+        db = config.get("db");
     } else if (environment_name === "server") {
         db = config.get("db_SERVER");
     } else {
@@ -28,6 +29,35 @@ const connectToDB = async () => {
 
 
 
+// تابع برای اضافه کردن رول‌ها
+async function addRolesIfNotExist() {
+    // لیست رول‌هایی که باید اضافه شوند
+    const newRoles = [
+        { _id: new mongoose.Types.ObjectId('663902a12733b1e14bcde2f4'), title: 'مسافر', active: true, is_static: false, rank: 'regular' },//passenger
+        { _id: new mongoose.Types.ObjectId('663902a02733b1e14bcde2ee'), title: 'راننده', active: true, is_static: false, rank: 'regular' },//driver
+        { _id: new mongoose.Types.ObjectId('663f11c3185aeaa232719f76'), title: 'مدیر پروژه', active: true, is_static: false, rank: 'regular' },//manager
+        { _id: new mongoose.Types.ObjectId('663f7ec2665933a1316d2697'), title: 'دیسپاچر', active: true, is_static: false, rank: 'regular' },//dispatcher
+        { _id: new mongoose.Types.ObjectId('664a047476905c9e69a259d0'), title: 'سوپر دیسپاچر', active: true, is_static: false, rank: 'regular' },//superDispatcher
+        { _id: new mongoose.Types.ObjectId('663902a12733b1e14bcde2f2'), title: 'مدیر سیستم', active: true, is_static: false, rank: 'admin' },//admin
+        { _id: new mongoose.Types.ObjectId('66605998eafc2a3a887c0dc4'), title: 'اپراتور', active: true, is_static: false, rank: 'regular' }//operator
+    ];
+    try {
+
+
+        for (const role of newRoles) {
+            await newuserRole.findOneAndUpdate(
+                { _id: role._id}, // شرط جستجو (مثلاً براساس _id)
+                { $set: role }, // به‌روزرسانی داده‌ها با newAccount
+                { upsert: true, new: true, setDefaultsOnInsert: true } // گزینه‌ها: ایجاد کاربر جدید اگر نبود
+            );
+
+        }
+    } catch (error) {
+        console.error(5478787, 'Error adding roles:', error);
+    }
+}
+
+
 exports.migrateDataAccounts = async function (req, res) {
     try {
         await connectToDB()
@@ -39,6 +69,9 @@ exports.migrateDataAccounts = async function (req, res) {
     try {
         const oldAccountsData = await old_accounts.find({})
         const oldUsersData = await old_users.find({})
+
+        addRolesIfNotExist()
+
 
         for (const oldAccountData of oldAccountsData) {
             try {
@@ -57,32 +90,32 @@ exports.migrateDataAccounts = async function (req, res) {
                 oldAccountData?.role?.map(role => {
                     switch (role) {
                         case 0: //passenger
-                            createNewRole.push(ObjectId('663902a12733b1e14bcde2f4'))
+                            createNewRole.push(new ObjectId('663902a12733b1e14bcde2f4'))
                             break;
 
                         case 1: //driver
                             const DB_ROLE_DRIVER_ID = '663902a02733b1e14bcde2ee'
-                            createNewRole.push(ObjectId(DB_ROLE_DRIVER_ID))
+                            createNewRole.push(new ObjectId(DB_ROLE_DRIVER_ID))
                             break;
 
                         case 2: //manager
-                            createNewRole.push(ObjectId('663f11c3185aeaa232719f76'))
+                            createNewRole.push(new ObjectId('663f11c3185aeaa232719f76'))
                             break;
 
                         case 3://dispatcher
-                            createNewRole.push(ObjectId('663f7ec2665933a1316d2697'))
+                            createNewRole.push(new ObjectId('663f7ec2665933a1316d2697'))
                             break;
 
                         case 4: //superDispatcher
-                            createNewRole.push(ObjectId('664a047476905c9e69a259d0'))
+                            createNewRole.push(new ObjectId('664a047476905c9e69a259d0'))
                             break;
 
                         case 5://admin
-                            createNewRole.push(ObjectId('663902a12733b1e14bcde2f2'))
+                            createNewRole.push(new ObjectId('663902a12733b1e14bcde2f2'))
                             break;
 
                         case 6://operator
-                            createNewRole.push(ObjectId('66605998eafc2a3a887c0dc4'))
+                            createNewRole.push(new ObjectId('66605998eafc2a3a887c0dc4'))
                             break;
                     }
 
@@ -107,12 +140,16 @@ exports.migrateDataAccounts = async function (req, res) {
                     // last_login_date:olduser.last_login_date
                 });
 
-                await newUserAccount(newAccount).save();
+                await newUserAccount.findOneAndUpdate(
+                    { _id: newAccount._id }, // شرط جستجو (مثلاً براساس _id)
+                    { $set: newAccount }, // به‌روزرسانی داده‌ها با newAccount
+                    { upsert: true, new: true, setDefaultsOnInsert: true } // گزینه‌ها: ایجاد کاربر جدید اگر نبود
+                );
             } catch (error) {
-                console.error(`Error migrating request ID ${oldAccountData._id}:`, error.message);
+                console.error(45468, `Error migrating request ID ${oldAccountData._id}:`, error.message);
                 res.status(500).json({
                     status: "5000",
-                    message: "Migration failed",
+                    message: "Migration failed2",
                     error: error.message,
                 });
             }
@@ -125,7 +162,7 @@ exports.migrateDataAccounts = async function (req, res) {
         console.error('Migration error:', error);
         res.status(500).json({
             status: "5000",
-            message: "Migration failed",
+            message: "Migration failed1",
             error: error.message,
         });
     } finally {
